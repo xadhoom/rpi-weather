70a67c387405b36e99061e050c5516793120f8a3
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2023-01-22 12:55:32 +0100

    Add sleep handling to pm25 sensor.
    Now the sensor read() expects to be called frequently
    since it will compute the needed times to wakeup the
    fan and perform the reading, which right now will be
    every 3 minutes (180 secs)

--- weather/sensors/pm25.py
+++ weather/sensors/pm25.py
@@ -1,25 +1,101 @@
 import logging
+import pigpio
+from datetime import datetime
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
 
+# Reading interval, in sec
+# Please note that datasheet suggests to wait at least 30
+# seconds after resuming, to let the fan properly
+# circulate the air in the chamber.
+READ_INTVL_SEC = 180
+# Delay in seconds between resuming from sleep
+# and performing the actual read. At least 30 seconds.
+SPINUP_DELAY_SEC = 45
+
 # TODO attach reset and set pins
 
-reset_pin = None
+# reset_pin = None
 # If you have a GPIO, its not a bad idea to connect it to the RESET pin
 # reset_pin = DigitalInOut(board.G0)
 # reset_pin.direction = Direction.OUTPUT
 # reset_pin.value = False
 
+# NOTE: we call 'em pins, but really they follows GPIOs numbers, see https://pinout.xyz
+
+IDLE = 0
+STARTING = 1
+
 
 class Pm25(object):
     _sensor = None
+    _reset_pin = None
+    _set_pin = None
+    _gpio_intf = None
+    _last_ts = None
+    _running = False
+    _state = IDLE
+    _read_interval_sec = READ_INTVL_SEC-SPINUP_DELAY_SEC
+    _spinup_delay_sec = SPINUP_DELAY_SEC
 
-    def __init__(self, i2c):
-        self._sensor = PM25_I2C(i2c, reset_pin)
+    def __init__(self, i2c, gpio_intf, reset_pin=None, set_pin=None):
+        self._sensor = PM25_I2C(i2c)
+        self._reset_pin = reset_pin
+        self._set_pin = set_pin
+        self._gpio_intf = gpio_intf
+
+        self.init_pins()
+        self.shutdown()
+
+        self._last_ts = self._utcnow()
 
         logging.info("PM2.5 ready")
 
+    def init_pins(self):
+        if self._reset_pin:
+            self._gpio_intf.set_mode(self._reset_pin, pigpio.OUTPUT)
+            self._gpio_intf.write(self._reset_pin, 1)
+
+        if self._set_pin:
+            self._gpio_intf.set_mode(self._set_pin, pigpio.OUTPUT)
+
+    def resume(self):
+        logging.debug("Resuming PM25 sensor from sleep")
+
+        if self._set_pin:
+            self._gpio_intf.write(self._set_pin, 1)
+
+        self._running = True
+        self._state = STARTING
+
+    def shutdown(self):
+        logging.debug("Putting PM25 sensor to sleep")
+
+        if self._set_pin:
+            self._gpio_intf.write(self._set_pin, 0)
+
+        self._running = False
+        self._state = IDLE
+
     def read(self):
+        elapsed = self._utcnow() - self._last_ts
+        logging.debug("Elapsed %r in state %s", elapsed, self._state)
+
+        if self._state == IDLE:
+            if elapsed >= self._read_interval_sec:
+                self.resume()
+                self._last_ts = self._utcnow()
+        elif self._state == STARTING:
+            if elapsed >= self._spinup_delay_sec:
+                self._read_impl()
+                self.shutdown()
+                self._last_ts = self._utcnow()
+
+    def _utcnow(self):
+        now = datetime.utcnow()
+        return now.timestamp()
+
+    def _read_impl(self):
         pm25 = self._sensor
 
         try:
