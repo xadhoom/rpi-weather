03325ce77a8abe6102d36ec9163f9fd26d238733
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2023-02-19 16:24:42 +0100

    add config file

---
+++ weather/config.sample.py
@@ -0,0 +1,8 @@
+config = {
+    "mqtt_pass": "SuperSecurePass",
+    "mqtt_user": "AnHeftyUser",
+    "mqtt_host": "mqtt.example.com",
+    "mqtt_ssl": True,
+    "mqtt_root_topic": "air-quality",
+    "sensor-id": "AnHeftySensor"
+}
