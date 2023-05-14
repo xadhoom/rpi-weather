81e30ee30aeec50027d90640faa94d58f91ef7d3
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2023-05-14 12:06:31 +0200

    Circuit-break scd41 crashes and use low power measurement.
    Seems that sometime the sdc41 fails, so is needed to escape
    from the loop otherwise the main loop will be blocked forever.
    Failure seems related to power issues, so we switch to
    low power measurement, but still from time to time fails,
    propably because main PSU cannot cope with 200mAh spikes during the read.
    Need to check if is a cable issues or we need to replace the sensor,
    maybe with the SCD30 which is a bit more costly but consumes less at peak
    (and is more precise)

--- weather/sensors/scd41.py
+++ weather/sensors/scd41.py
@@ -2,6 +2,8 @@ import adafruit_scd4x
 import time
 import logging
 
+DATA_READY_RETRIES = 3
+
 
 class Scd41(object):
     _sensor = None
@@ -15,20 +17,27 @@ class Scd41(object):
         self._press_cb = press_fun
         self._store = store
         logging.debug("Serial number: %s", self.serial)
-
-        self._sensor.stop_periodic_measurement()  # if any
-        self._sensor.factory_reset()
-        self._sensor.start_periodic_measurement()
-
+        self._reset(True)
         logging.info("SCD41 ready")
 
+    def _reset(self, factory):
+        logging.info("Resetting sensor, factory: %s", factory)
+        self._sensor.stop_periodic_measurement()  # if any
+        if factory:
+            self._sensor.factory_reset()
+        self._sensor.start_low_periodic_measurement()
+        #self._sensor.start_periodic_measurement()
+
     def read(self):
+        logging.debug("--- START SCD41 read ---")
         scd4x = self._sensor
+        retries = 0
 
         scd4x.set_ambient_pressure(int(self._press_cb()))
 
         while True:
-            if scd4x.data_ready:
+            retries = retries + 1
+            if scd4x.data_ready and retries <= DATA_READY_RETRIES:
                 co2 = scd4x.CO2
                 temperature = scd4x.temperature
                 humidity = scd4x.relative_humidity
@@ -41,6 +50,13 @@ class Scd41(object):
                 logging.debug("Temperature: %0.1f *C", temperature)
                 logging.debug("Humidity: %0.1f %%", humidity)
                 break
+            elif retries > DATA_READY_RETRIES:
+                logging.warning(
+                    "Too many retries for data ready status, skipping to next cycle")
+                break
             else:
-                logging.debug("Waiting for first measurement....")
+                logging.info("Waiting for first measurement (%d of %d)....",
+                             retries, DATA_READY_RETRIES)
                 time.sleep(1)
+
+        logging.debug("--- END SCD41 read ---")
